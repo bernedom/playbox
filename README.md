e39065428e66a95b84fc1ecea68e333b2fc01981
Dominik Berner <dominik.berner@gmail.com>
2020-03-21 22:43:25 +0100

    Mapping code to ascii representation

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ RFID player for raspberry pi
 
 # Hints for myself
 user should be memeber of group `input` to access devices
+evdev documentation
+https://python-evdev.readthedocs.io/en/latest/
--- src/RFID_reader.py
+++ src/RFID_reader.py
@@ -4,23 +4,22 @@ import sys
 import evdev
 from evdev import InputDevice, ecodes, list_devices
 
-
-def get_devices():
-    return [InputDevice(fn) for fn in list_devices()]
-
-
-print(get_devices())
-
-devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-for device in devices:
-    print(device.path, device.name, device.phys)
-
-
 device = evdev.InputDevice('/dev/input/event6')
-print(device.capabilities(verbose=True))
+# print(device.capabilities(verbose=True))
 
+scancodes = {
+    # Scancode: ASCIICode
+    0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8',
+    10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'Q', 17: u'W', 18: u'E', 19: u'R',
+    20: u'T', 21: u'Y', 22: u'U', 23: u'I', 24: u'O', 25: u'P', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
+    30: u'A', 31: u'S', 32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 39: u';',
+    40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N',
+    50: u'M', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 100: u'RALT'
+}
 
 for event in device.read_loop():
     # print(evdev.categorize(event))
-    if event is evdev.KeyEvent:
-        print(event.keycode)
+    cat_event = evdev.categorize(event)
+    if hasattr(cat_event, "keycode"):
+        print("KC" + scancodes[cat_event.scancode])
+        #print("VAL: " + str(ecodes.ecodes[event.code]))
