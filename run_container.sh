d48c090a65c5c90d2ba08507c5350191e6bc618e
Dominik Berner <dominik.berner@gmail.com>
2020-03-29 23:34:39 +0200

    Exposing port for web service

--- run_container.sh
+++ run_container.sh
@@ -3,4 +3,4 @@
 docker run -v $(pwd):/root/mount \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -e DISPLAY=$DISPLAY -d \
---device /dev/snd --privileged -p 6680:6680 --rm phoniedocker
+--device /dev/snd --privileged -p 10000:10000 --rm phoniedocker
