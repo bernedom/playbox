9e2e1937ee41cb7b99a712cc062fc929069fece3
Dominik Berner <dominik.berner@gmail.com>
2020-04-14 22:55:39 +0200

    Handling relative paths

--- playbox/AudioLibrary.py
+++ playbox/AudioLibrary.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
+import validators
+import pathlib
+import logging
+
 
 class AudioLibrary:
 
     def __init__(self):
         self.__audio = {}
 
-    def registerAudio(self, key: str, audio_uri: str):
-        self.__audio[key] = audio_uri
+    def registerAudio(self, key: str, audio_path: str):
+        if(not pathlib.Path(audio_path).is_absolute()):
+            audio_path = pathlib.Path(audio_path).absolute()
+
+        logging.debug(
+            "Matching key {} to audio file {}".format(key, audio_path))
+        self.__audio[key] = pathlib.Path(audio_path).as_uri()
 
     def getAudio(self, key: str):
         return self.__audio[key]
--- test/test_library.py
+++ test/test_library.py
@@ -4,18 +4,32 @@ import pytest
 import playbox
 
 
-def test_song_retrival_by_number_if_number_is_present():
+def test_audio_retrival_by_number_if_number_is_present():
     # set up
     library = playbox.AudioLibrary()
-    library.registerAudio("12345", "file:///my/song/foo.mp3")
+    library.registerAudio("12345", "/my/audio/foo.ogg")
 
     uri = library.getAudio("12345")
-    assert uri == "file:///my/song/foo.mp3"
+    assert uri == "file:///my/audio/foo.ogg"
     assert library.hasAudio("12345")
 
 
-def test_song_retrieval_by_number_if_number_is_not_present():
+def test_audio_retrieval_by_number_if_number_is_not_present():
     library = playbox.AudioLibrary()
     assert not library.hasAudio("12345")
     with pytest.raises(KeyError):
         library.getAudio("12345")
+
+
+def test_if_audio_added_as_path_then_is_converted_to_uri():
+    library = playbox.AudioLibrary()
+    library.registerAudio("12345", "/my/audio/foo.ogg")
+
+    assert "file:///my/audio/foo.ogg" == library.getAudio("12345")
+
+
+def test_if_audio_added_as_relative_path_is_converted_to_uri():
+    library = playbox.AudioLibrary()
+    library.registerAudio("12345", "./somefile.ogg")
+
+    assert library.getAudio("12345").startswith("file://")
