fb00d561861aa7f5c3aa11e16238f3868d6e65dc
Dominik Berner <dominik.berner@gmail.com>
2020-04-30 16:05:39 +0200

    Saving audio files in /var

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from playbox import AudioLibrary, Player, RFID_Reader
 library = AudioLibrary()
 
 # temporary set up
-library.registerAudio("03242743", "/root/mount/test.MP3")
+library.registerAudio("03242743", "/var/playbox/audio/test.MP3")
 # library.registerAudio("99999999", "spotify:track:0tKcYR2II1VCQWT79i5NrW")
 library.registerSpotifyAudio(
     "03046971", "https://open.spotify.com/track/1zB4vmk8tFRmM9UULNzbLB")
@@ -19,4 +19,3 @@ player = Player(library)
 reader = RFID_Reader(player)
 reader.aqcuireDevice("HID 413d:2107")
 reader.run()
-
