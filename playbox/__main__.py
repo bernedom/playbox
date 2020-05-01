f01a792a7ef1508d6d872d6a2b8c6545fe07da77
Dominik Berner <dominik.berner@gmail.com>
2020-05-01 21:44:13 +0200

    Remove duplicate code

--- playbox/__main__.py
+++ playbox/__main__.py
@@ -4,25 +4,9 @@ from playbox.RFID_Reader import RFID_Reader
 from playbox.Player import Player
 from playbox.AudioLibrary import AudioLibrary
 
-library = AudioLibrary()
 
-# temporary set up
-library.registerAudio("03242743", "/root/mount/test.MP3")
-# library.registerAudio("99999999", "spotify:track:0tKcYR2II1VCQWT79i5NrW")
-library.registerSpotifyAudio(
-    "03046971", "https://open.spotify.com/track/1zB4vmk8tFRmM9UULNzbLB")
-library.saveToCsv("/var/playbox/music.csv")
-#
-
-library.readFromCsv("/var/playbox/music.csv")
-
-
-player = Player(library)
-reader = RFID_Reader(player)
-reader.aqcuireDevice("HID 413d:2107")
-reader.run()
-
-
+# TODO Move player connect out of RFID Reader
+# TODO Remove sleep(5.) and loop until connection or timeout
 # TODO stop playing when shutting down
 # TODO read library from file
 # TODO try spotify albums, playlists etc
