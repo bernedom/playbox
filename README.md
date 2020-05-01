db0a7b75bbead3fb1c55f308c5f1cbd18d3ba518
Dominik Berner <dominik.berner@gmail.com>
2020-05-01 23:05:31 +0200

    Including ready sound and config into python package

--- README.md
+++ README.md
@@ -38,6 +38,8 @@ user should be member of group `input` to access devices
 evdev documentation
 https://python-evdev.readthedocs.io/en/latest/
 
+python packaging help: https://docs.python.org/3/distutils/setupscript.html
+
 Debian packaging intro: https://wiki.debian.org/Packaging/Intro?action=show&redirect=IntroDebianPackaging
 
 # Setting up VirtualEnv
--- setup.py
+++ setup.py
@@ -6,13 +6,19 @@ __author__ = 'Dominik Berner'
 
 setup(
     name='playbox',
-    version='0.0.1-alpha',
+    version='0.0.1a0',
     description='RFID player for raspberry pi. Using Mopidy/MPD to play back songs triggered by RFID cards.',
     author='Dominik Berner',
     author_email='dominik.berner@gmail.com',
-    license='GNU',
+    license='GPLv3',
     url='https: // github.com/bernedom/playbox',
     packages=['playbox'],
     entry_points={'console_scripts': ['tvb=tvb:main']},
-    scripts=['run.py']
+    scripts=['run.py'],
+
+    #package_data={'playbox': ['ready.mp3']}
+    data_files=[('resource', ['resource/ready.mp3',
+                              'resource/mopidy.conf', 'resource/playbox.service'])]
+
+
 )
