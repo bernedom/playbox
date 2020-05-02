77fcad3e9e26f653f4049660044fe8277f2dba47
Dominik Berner <dominik.berner@gmail.com>
2020-05-02 11:34:02 +0200

    Add stop token

--- run.py
+++ run.py
@@ -9,7 +9,9 @@ library.readFromCsv("/var/playbox/audio.csv")
 # TODO  sleep because of race condition on startup
 sleep(5.0)
 
+# TODO move configuration to a config file and create setup routine
 player = Player(library)
+player.registerStop("03331943")
 reader = RFID_Reader(player)
 reader.aqcuireDevice("HID 413d:2107")
 player.playuri("file:///opt/playbox/ready.mp3")
