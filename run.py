c36fb1688e0c2431b479f9d9a667b163aafd83dd
Dominik Berner <dominik.berner@gmail.com>
2020-05-02 11:00:59 +0200

    Do not create csv on startup

--- run.py
+++ run.py
@@ -5,15 +5,7 @@ from playbox import AudioLibrary, Player, RFID_Reader
 
 library = AudioLibrary()
 
-# temporary set up
-library.registerAudio("03242743", "/var/playbox/audio/test.MP3")
-# library.registerAudio("99999999", "spotify:track:0tKcYR2II1VCQWT79i5NrW")
-library.registerSpotifyAudio(
-    "03046971", "https://open.spotify.com/track/1zB4vmk8tFRmM9UULNzbLB")
-library.saveToCsv("/var/playbox/music.csv")
-#
-
-library.readFromCsv("/var/playbox/music.csv")
+library.readFromCsv("/var/playbox/audio.csv")
 # TODO  sleep because of race condition on startup
 sleep(5.0)
 
