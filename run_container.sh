5eb82e9c37686bc977ff4f4ebe586ae9651f3ab0
Dominik Berner <dominik.berner@gmail.com>
2020-09-30 22:37:35 +0200

    Renamed container to playbox

--- run_container.sh
+++ run_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#docker run -v $(pwd):/root/mount -ti  --device /dev/snd phoniedocker
+
 docker run -ti -v $(pwd):/root/mount \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v $(pwd)/resource/audio.csv:/var/playbox/audio.csv \
 -e DISPLAY=$DISPLAY -d \
---device /dev/snd --privileged -p 10000:10000 --rm phoniedocker
+--device /dev/snd --privileged -p 10000:10000 --rm playbox
