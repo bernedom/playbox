a3e90e6c9d57b75c172fa95e679d486922243ef9
Dominik Berner <dominik.berner@gmail.com>
2020-11-07 21:55:55 +0100

    Spotify-configuration is passed runtime only

--- build_container.sh
+++ build_container.sh
@@ -4,14 +4,16 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # create python package
 source .venv/bin/activate
-python3 setup.py sdist 
+python3 setup.py sdist
 source .venv/bin/deactivate
 
 source ${CURRENT_DIR}/.vscode/source_spotify.sh
+echo "[spotify]" > ${CURRENT_DIR}/.vscode/spotify.conf
+echo "password = ${SPOTIFY_PASS}" >> ${CURRENT_DIR}/.vscode/spotify.conf
+echo "username = ${SPOTIFY_USER}" >> ${CURRENT_DIR}/.vscode/spotify.conf
+echo "client_id = ${SPOTIFY_CLIENT_ID}" >> ${CURRENT_DIR}/.vscode/spotify.conf
+echo "client_secret = ${SPOTIFY_CLIENT_SECRET}" >> ${CURRENT_DIR}/.vscode/spotify.conf
 
-docker build . -t playbox --rm --build-arg SPOTIFY_USER=${SPOTIFY_USER} \
---build-arg SPOTIFY_PASS=${SPOTIFY_PASS} \
---build-arg SPOTIFY_CLIENT_ID=${SPOTIFY_CLIENT_ID} \
---build-arg SPOTIFY_CLIENT_SECRET=${SPOTIFY_CLIENT_SECRET}
+docker build . -t playbox --rm
 
 
--- run_container.sh
+++ run_container.sh
@@ -2,7 +2,10 @@
 
 docker run -ti -v $(pwd):/root/mount \
 -v $(pwd)/resource/audio.csv:/var/playbox/audio.csv \
+-v $(pwd)/.vscode/spotify.conf:/etc/mopidy/spotify.conf \
 -v /dev/bus/usb:/dev/bus/usb \
 --device /dev/snd \
 -e PLAYBOX_DEBUG=-d \
 --privileged -p 10000:10000 --rm playbox
+
+
